8b9b77e0ad98f249dfb8f2ef8e282f5a17afb6a1
GMELUM <gmelum@vk.com>
2024-02-01 09:52:57 +0300

    review Inconsistent

--- Inconsistent.go
+++ Inconsistent.go
@@ -1,15 +1,18 @@
 package msg
 
-var errorProducsNotFount = serialize(errorData{
-	Code:     20,
-	Message:  "There is no such thing as a product or subscription.",
+var errorInconsistent = serialize(errorData{
+	Code:     11,
+	Message:  "The request is missing one or more mandatory fields",
 	Critical: true,
 })
 
 //	Use in shop VK callback.
 //
-//	The item or subscription does not exist.
-func ProducsNotFount(ctx context) error {
-	ctx.Write(errorProducsNotFount)
+//	One of the following reasons:
+//		• The request parameters do not match the specification.
+//		• One or more mandatory fields are missing in the request.
+//		• Request integrity errors.
+func Inconsistent(ctx context) error {
+	ctx.Write(errorInconsistent)
 	return nil
 }
